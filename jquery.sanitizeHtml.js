0aa97706ab32a3e2f746d483ef45592be0c87500
Harlan Iverson <h.iverson@gmail.com>
2010-06-27 17:58:34 -0500

    added attribute support

--- jquery.sanitizeHtml.js
+++ jquery.sanitizeHtml.js
@@ -10,6 +10,10 @@
 
 
 var defaultAllowTags = ['i', 'em', 'b', 'strong', 'u', 'font'];
+// key = tag, value = attr
+var defaultAllowAttrs = {
+	'font': ['face', 'color', 'size'],
+}
 
 /*
 Function: sanitizeChildren
@@ -25,19 +29,32 @@ Parameters:
 	root - A DOM node or anything that will work in a jQuery selector. This
 			node is not sanitized; only its children.
 	allowTags - An array of tags to allow. Default is: i, em, b, strong, u, font.
+	allowAttrs - A map of tag => [attr] to allow. Default is {'font' : ['face', 'color', size']}.
 */
 
-function sanitizeChildren( root, allowTags ) {
+function sanitizeChildren( root, allowTags, allowAttrs ) {
 	allowTags = allowTags || defaultAllowTags;
+	allowAttrs = allowAttrs || defaultAllowAttrs;
 
 	$(root).children().each(function(i, child) {
 		sanitizeChildren( child, allowTags );
 
-		if( $.inArray( child.localName, allowTags ) < 0 ) {
+		var tag = child.localName.toLowerCase();
+
+		if( $.inArray( tag, allowTags ) < 0 ) {
 			$(child).contents().each(function(j, content) {
 				$(content).remove().insertBefore(child);
 			});
 			$(child).remove();
+		} else {
+			$.each(child.attributes, function() {
+				var attr = this.localName;
+
+				if( !defaultAllowAttrs.hasOwnProperty(tag) 
+					|| $.inArray( attr, defaultAllowAttrs[tag] ) < 0 ) {
+					child.removeAttribute( attr );
+				}
+			});
 		}
 	});
 };
@@ -49,14 +66,21 @@ Function: jQuery.fn.sanitizeHtml
 	Sanitizes the children of a given node. The root node is assumed to be
 	a safe container so it is not sanitized itself; only the children. All 
 	text is preserved (unlike with regex replacement) and kept in the proper
-	place.
+	place. For example, the HTML
 
-	Note: jQuery itself handles removal of <script> tags, and I don't have 
-	the immediate need to allow them. If you know/find a solution please let 
-	me know and I'll note it here.
+	: <div><table><tr><td><p>Sup <b class='awesome'>dog</b><font color="red" class="awesomer">!</font> <em>I like dogs</em></p></td></tr></table> wasn't that table cool?</div>
+
+	becomes:
+
+	: <div>Sup <b>dog</b><font color="red">!</font> <em>I like dogs</em> wasn't that table cool?</div>
+
+	Note: jQuery itself handles removal of some tags tags, such as <script>, 
+	and I don't have the immediate need to allow them. If you know/find a solution 
+	please let me know and I'll note it here.
 
 Parameters:
 	allowTags - An array of tags to allow. Default is: i, em, b, strong, u, font.
+	allowAttrs - A map of tag => [attr] to allow. Default is {'font' : ['face', 'color', size']}.
 
 Example:
 
@@ -67,10 +91,13 @@ Example:
 */
 
 
-$.fn.sanitizeHtml = function(allowTags) {
+$.fn.sanitizeHtml = function(allowTags, allowAttrs) {
 	allowTags = allowTags || defaultAllowTags;
+	allowAttrs = allowAttrs || defaultAllowAttrs;
 
-	sanitizeChildren( $(this), allowTags );
+	sanitizeChildren( this, allowTags );
+
+	return $(this);
 }
 
 })(jQuery);
